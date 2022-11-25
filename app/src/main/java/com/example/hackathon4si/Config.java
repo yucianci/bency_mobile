5e152577a23483799c87c7b34580e314007c48c2
dennysbueno <dennys.bueno8@gmail.com>
2022-11-25 07:18:37 -0300

    :sparkles: feat: added main page

--- app/src/main/java/com/example/hackathon4si/Config.java
+++ app/src/main/java/com/example/hackathon4si/Config.java
@@ -2,5 +2,5 @@ package com.example.hackathon4si;
 
 public class Config {
     public static String urlApi = "https://pokeapi.co/api/v2/pokemon";
-    
+    //public static String linkImagem = ""
 }
--- app/src/main/java/com/example/hackathon4si/MainActivity.java
+++ app/src/main/java/com/example/hackathon4si/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.hackathon4si;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.Nullable;
+
+import com.example.hackathon4si.datasources.DadosApi;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends ListActivity {
+
+   private ArrayList<HashMap<String, String>> listaDados;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            listaDados = new DadosApi().execute(Config.urlApi).get();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String texto = listaDados.get(0).get("nome");
+        Toast.makeText(this, texto, Toast.LENGTH_LONG).show();
+
+        ListAdapter adapter = new SimpleAdapter(
+                this,
+                listaDados,
+                R.layout.listview_modelo,
+                new String[] { "nome" },
+                new int[] { R.id.txtNome }
+        );
+
     }
 }
